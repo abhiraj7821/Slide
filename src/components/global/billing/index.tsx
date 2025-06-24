71760b41a90804c873f406b9c2fb58b36f80bddf
Abhishek Rajput <68955350+abhiraj7821@users.noreply.github.com>
2025-06-24 12:35:38 +0530

    Integrate user subscription data in Billing component
    Added useQueryUser hook to fetch user data and updated PaymentCard components to display the current subscription plan dynamically based on fetched data. Improved layout classes for better alignment.

--- src/components/global/billing/index.tsx
+++ src/components/global/billing/index.tsx
@@ -1,14 +1,19 @@
+'use client'
 import React from 'react'
 import PaymentCard from './payment'
+import { useQueryUser } from '@/hooks/user-queries'
 
 type Props = {}
 
 function Billing({}: Props) {
 //   WIP: Fetch billing information for the customer
+
+const {data} = useQueryUser()
+
   return (
-    <div className='flex lg:flex-row flex-col gap-5 w-full lg:2-10/12 xl:w-8/12 container '>
-        <PaymentCard current={'FREE'} label='FREE'/>
-        <PaymentCard current={'FREE'} label='PRO'/>
+    <div className='flex lg:flex-row flex-col items-center lg:items-start gap-5 w-full lg:w-10/12 xl:w-8/12 container '>
+        <PaymentCard current={data?.data?.subscription?.plan} label='PRO'/>
+        <PaymentCard current={data?.data?.subscription?.plan} label='FREE'/>
     </div>
   )
 }
