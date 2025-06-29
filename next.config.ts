2ee60d8525b2d0b12e63c8a706bc04a5ef55acf9
Abhishek Rajput <68955350+abhiraj7821@users.noreply.github.com>
2025-06-29 23:18:32 +0530

    Add remote image patterns to Next.js config
    Configured Next.js to allow images from 'instagram.frpr1-1.fna.fbcdn.net' and 'scontent.cdninstagram.com' by adding them to the images.remotePatterns array in next.config.ts.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images:{
+    remotePatterns:[
+      {
+        protocol: 'https',
+        hostname: 'instagram.frpr1-1.fna.fbcdn.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'scontent.cdninstagram.com'
+      }
+    ]
+  }
   /* config options here */
 };
 
