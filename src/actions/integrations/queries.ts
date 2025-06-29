5d69dcc042528614f3348ed7f55c63c2c393bf1c
Abhishek Rajput <68955350+abhiraj7821@users.noreply.github.com>
2025-06-29 23:18:26 +0530

    Add Instagram integration logic and queries
    Implemented async OAuth and integration flow for Instagram, including token generation, user ID retrieval, and integration creation. Added getIntegration and createIntegration queries to manage user integrations in the database.

--- src/actions/integrations/queries.ts
+++ src/actions/integrations/queries.ts
@@ -15,3 +15,48 @@ export const updateIntegration = async (token:string, expire: Date, id:string)=>
         console.log("Error at update Integrations",error);
     }
 }
+
+export const getIntegration = async (clerkId:string)=>{
+    return await client.user.findUnique({
+        where:{
+            clerkId,
+        },
+        select:{
+            integrations:{
+                where:{
+                    name:'INSTAGRAM',
+                }
+            }
+        }
+    })
+}
+
+export const createIntegration = async (
+    clerkId:string,
+    token:string,
+    expire: Date,
+    instaId:string,
+)=>{
+    try {
+        return await client.user.update({
+            where:{
+                clerkId,
+            },
+            data:{
+                integrations:{
+                    create:{
+                        token,
+                        expiresAt:expire,
+                        instagramId: instaId,
+                    }
+                }
+            },
+            select:{
+                firstname:true,
+                lastname:true,
+            }
+        })
+    } catch (error) {
+        console.log("Error at create Integrations",error);
+    }
+}
